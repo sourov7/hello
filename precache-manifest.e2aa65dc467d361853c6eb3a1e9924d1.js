43351626ec249f9615f2cfc11da17ab31e8c3315
sourov7 <37473187+sourov7@users.noreply.github.com>
2018-10-17 19:45:49 +0600

    Updates

---
+++ precache-manifest.e2aa65dc467d361853c6eb3a1e9924d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "6b21cf0f35aee299a108",
+    "url": "/hello/static/js/main.6b21cf0f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/hello/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6b21cf0f35aee299a108",
+    "url": "/hello/static/css/main.c7917151.chunk.css"
+  },
+  {
+    "revision": "34eb1897deab6077b65e00716ba8173a",
+    "url": "/hello/index.html"
+  }
+];
